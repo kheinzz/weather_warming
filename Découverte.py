a7241179e2aeb17d4718997a9ee40fe093f86e75
Karl Heinz <46902922+kheinzz@users.noreply.github.com>
2025-06-18 09:41:32 +0200

    Update Découverte.py

--- Découverte.py
+++ Découverte.py
@@ -147,17 +147,17 @@ def process_data(file_path_liste, file_path_sh_rr, key_nb):
 ############    TAB 1     ############
 #######################################
 # with tab1 : 
-st.subheader("Bienvenue sur weather warming, cette appli vise à utiliser et valoriser les données ouvertes de [Météo-France](https://meteo.data.gouv.fr/).")
-st.write("Compte-tenu de la prégnance du changement climatique dans notre quotidien ainsi que dans les médias et les travaux scientifiques, l'accent sera mis sur les séries temporelles de données climatologiques dites \"de référence pour l'étude du changement climatique\".")
-st.write("Dans un premier temps nous souhaitons explorer ce que renferment ces données, quels sont les fichiers et les variables qu'elles contiennent afin de donner du contexte au reste de l'application et au traitements qui seront réalisés.")
+st.subheader("Cette appli vise à utiliser et valoriser les données ouvertes de [Météo-France](https://meteo.data.gouv.fr/).")
+st.write("L'accent sera mis sur les séries temporelles de données climatologiques dites \"de référence pour l'étude du changement climatique\".")
+st.write("Dans un premier temps je souhaitais explorer ce que renferment ces données, quels sont les fichiers et les variables qu'elles contiennent afin de donner du contexte au reste de l'application et au traitements qui seront réalisés.")
 st.write("""Le reste de l'application sera consacré à l'exploration de ces données ainsi qu'à des indicateurs tentant de mettre en exergue le changement climatique, mais aussi les implications de ce dernier sur nos territoires. 
             En effet, que ce soit du point de vue des températures ou des précipitations, le réchauffement du climat ne semble pas avoir les mêmes conséquences selon les territoires.
             """)
-with st.expander("warnings") : 
+with st.expander("Mise en garde") : 
     st.write("""Une première mise en garde doit être faite concernant cette série temporelle au regard de son objectif. En effet, le changement climatique
             ou tout du moins ses origines et ses causes prennent racine dans la période de la révolution industrielle et le début de l'emploi massif de ressources fossiles 
             comme sources d'énergie. Si l'on sait que les quantités de gaz à effet de serre n'ont fait qu'augmenter depuis cette période, le jeu de données étudié au travers de 
-            cette application ne permettra pas d'observer l'évolution du climat jusqu'à cette période puisque les séries commencent à partir des années 50 (vous le verrez, chaque série a sa date de début / fin)
+            cette application ne permettra pas d'observer l'évolution du climat depuis cette période puisque les séries commencent à partir des années 50 (vous le verrez, chaque série a sa date de début / fin)
             """)
 st.subheader("Les séries étudiées : ")
 
